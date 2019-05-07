f75c2cddce73b382ad8fb1b827020da24bb65fde
Bẩn <levietduc1108@gmail.com>
2019-05-07 10:24:14 +0700

    Coredata save Photo okay let go

--- CoreData_Photo/ViewController.swift
+++ CoreData_Photo/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    var tableVC = TableViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //Dismiss the picker neu nguoi chon cancel
+        dismiss(animated: true, completion: nil)
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        //dat imageView der hien thi hinh anh
+        imageView.image = selectedImage
+        // Dismiss the picker
+        dismiss(animated: true, completion: nil)
+    }
+    //MARK: Actions
+    @IBAction func selectImageFromLibrary(_ sender: UITapGestureRecognizer) {
+        //UIImagePickerController la mot view controller cho phep nguoi dung chon media tu thu vien anh cua ho
+        let imagePickerController = UIImagePickerController()
+        
+        //chi cho phep chon anh , hong chup
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func btnAdd(_ sender: Any) {
+        if let content = imageView.image {
+            let data = Entity(context: AppDelegate.context)
+            data.image = content
+            AppDelegate.saveContext()
+            self.tableVC.fetchObject()
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func btnCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
